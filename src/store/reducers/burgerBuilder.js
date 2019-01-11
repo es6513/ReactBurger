aff452320abcd7fed21467d53f3af369f0c00b92
es6513 <es6513@gmail.com>
2019-01-11 21:51:03 +0800

    React ReduxAdvanced Finished

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -20,30 +20,57 @@ const round = (value,decimals)=>{
 };
 
 const addIngredient = (state,action)=>{
-	
+	const updatedIngredient = {	[action.ingredientName]:state.ingredients[action.ingredientName] + 1};
+	const updatedIngredients = updateObject(state.ingredients,updatedIngredient);
+	const updatedState = {
+		ingredients:updatedIngredients,
+		totalPrice:round(state.totalPrice + INGREDIENT_PRICES[action.ingredientName],1)
+	};
+	return updateObject(state,updatedState);
+	//for update data in immutable way
+};
+
+const removeIngredient = (state,action)=>{
+	const updatedIng = {	[action.ingredientName]:state.ingredients[action.ingredientName] - 1};
+	const updatedIngs = updateObject(state.ingredients,updatedIng);
+	const updatedSt = {
+		ingredients:updatedIngs,
+		totalPrice:round(state.totalPrice - INGREDIENT_PRICES[action.ingredientName],1)
+	};
+	return updateObject(state,updatedSt);
+};
+
+const setIngredients = (state,action)=>{
+	let addtionalPrice = 0;
+	for(let ings in action.ingredients){
+		addtionalPrice = addtionalPrice + action.ingredients[ings] * [INGREDIENT_PRICES[ings]];
+	}
+	return updateObject(state,{
+		ingredients:{
+			salad:action.ingredients.salad,
+			bacon:action.ingredients.bacon,
+			cheese:action.ingredients.cheese,
+			meat:action.ingredients.meat
+		},
+		totalPrice:4 + addtionalPrice,
+		error:false
+	});
+};
+
+const fetchIngredientsFailed = (state,action)=>{
+	return updateObject(state,{
+		...state,
+		error:true
+	});
 };
 
 const reducer = (state = initialState,action)=>{
 	switch(action.type){
 
-	case actionTypes.ADD_INGREDIENT:
-		const updatedIngredient = {	[action.ingredientName]:state.ingredients[action.ingredientName] + 1};
-		const updatedIngredients = updateObject(state.ingredients,updatedIngredient);
-		const updatedState = {
-			ingredients:updatedIngredients,
-			totalPrice:round(state.totalPrice + INGREDIENT_PRICES[action.ingredientName],1)
-		};
-		return updateObject(state,updatedState);
-		//for update data in immutable way
-	case actionTypes.REMOVE_INGREDIENT:
-		const updatedIng = {	[action.ingredientName]:state.ingredients[action.ingredientName] - 1};
-		const updatedIngs = updateObject(state.ingredients,updatedIng);
-		const updatedSt = {
-			ingredients:updatedIngs,
-			totalPrice:round(state.totalPrice - INGREDIENT_PRICES[action.ingredientName],1)
-		};
-		return updateObject(state,updatedSt);
-
+	case actionTypes.ADD_INGREDIENT: return addIngredient(state,action);
+		
+	case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state,action);
+	
 	//other way to update price 
 	case actionTypes.UPDATE_PRICE:
 		
@@ -59,27 +86,9 @@ const reducer = (state = initialState,action)=>{
 			...state,
 			totalPrice:updatedPrice
 		};
-	
-	case actionTypes.SET_INGREDIENTS:
-		let addtionalPrice = 0;
-		for(let ings in action.ingredients){
-			addtionalPrice = addtionalPrice + action.ingredients[ings] * [INGREDIENT_PRICES[ings]];
-		}
-		return updateObject(state,{
-			ingredients:{
-				salad:action.ingredients.salad,
-				bacon:action.ingredients.bacon,
-				cheese:action.ingredients.cheese,
-				meat:action.ingredients.meat
-			},
-			totalPrice:4 + addtionalPrice,
-			error:false
-		});
-	case actionTypes.FETCH_INGREDIENTS_FAILED:
-		return updateObject(state,{
-			...state,
-			error:true
-		});
+
+	case actionTypes.SET_INGREDIENTS: return setIngredients(state,action);
+	case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state,action);
 	default:
 		return state;
 	}
